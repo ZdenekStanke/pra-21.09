a4998fab0c00ded125f15d2cdc3a00a257573fc5
zdenek.stanke <stankezdenek@seznam.cz>
2021-09-21 14:42:40 +0200

    Zajda neda klik

--- src/skola.java
+++ src/skola.java
@@ -2,5 +2,6 @@ public class skola {
     public static void main(String[] args) {
         System.out.println("ahoj svete");
         System.out.println("zelenej je ondra ");
+        System.out.println("zelenej je ondra ");
     }
 }
